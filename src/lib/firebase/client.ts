c8eddbc42781b3fe6a02f1b96d7c587f7e2db811
Pedro Laguna <pdr.laguna@gmail.com>
2023-09-12 14:29:28 -0300

    trying to build this

--- src/lib/firebase/client.ts
+++ src/lib/firebase/client.ts
@@ -1,12 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD8bSsEcYrV7Whgf8L6UD_P4M9SR_jb81k",
-  authDomain: "astro-auth-6cc43.firebaseapp.com",
-  projectId: "astro-auth-6cc43",
-  storageBucket: "astro-auth-6cc43.appspot.com",
-  messagingSenderId: "469295571959",
-  appId: "1:469295571959:web:20b9159287f8113f2744f2",
+  apiKey: "AIzaSyA8VnSRTgxlBFN3UQqV_FQXL0Yu8SUx1Is",
+  authDomain: "astro-hello-world.firebaseapp.com",
+  projectId: "astro-hello-world",
+  storageBucket: "astro-hello-world.appspot.com",
+  messagingSenderId: "600435031990",
+  appId: "1:600435031990:web:74a3731c68cf4178358b17",
+  measurementId: "G-1R1FM70D0J"
 };
 
 export const app = initializeApp(firebaseConfig);
--- src/lib/firebase/server.ts
+++ src/lib/firebase/server.ts
@@ -9,13 +9,13 @@ const serviceAccount = {
   private_key_id: import.meta.env.FIREBASE_PRIVATE_KEY_ID,
   private_key: import.meta.env.FIREBASE_PRIVATE_KEY,
   client_email:
-    "firebase-adminsdk-rap76@astro-auth-6cc43.iam.gserviceaccount.com",
-  client_id: "100068165142027748424",
+    "firebase-adminsdk-sog5j@astro-hello-world.iam.gserviceaccount.com",
+  client_id: "115563795714089678039",
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
   client_x509_cert_url:
-    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-rap76%40astro-auth-6cc43.iam.gserviceaccount.com",
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-sog5j%40astro-hello-world.iam.gserviceaccount.com",
 };
 
 export const app = initializeApp({
